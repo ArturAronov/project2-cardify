5684d0cfbd3ec78540a6bbecfcadfa42dfbcabd1
ArturAronov <unionco@icloud.com>
2022-06-27 15:43:18 +0800

    added not found page, commented router page

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,31 @@
 import { Router } from 'express'
+import authenticateUser from './_middlewares/authenticate-user.js'
 
 const router = Router()
 
+// PAGES | AUTH
 router.get('/', (await import('./controllers/pages/auth/login.js')).default)
 router.get('/auth/signup', (await import('./controllers/pages/auth/signup.js')).default)
-router.get('/my/profile', (await import('./controllers/pages/my/profile/show.js')).default)
-router.get('/my/profile/edit', (await import('./controllers/pages/my/profile/edit.js')).default)
-router.get('/my/collections', (await import('./controllers/pages/my/collections/show.js')).default)
-router.get('/my/collections/new', (await import('./controllers/pages/my/collections/new.js')).default)
-router.get('/my/collections/:id/edit', (await import('./controllers/pages/my/collections/edit.js')).default)
-router.get('/my/collections/:id/flashcards', (await import('./controllers/pages/my/flashcards/show.js')).default)
-router.get('/my/collections/:id/new', (await import('./controllers/pages/my/flashcards/new.js')).default)
-router.get('/flashcards/:id/edit', (await import('./controllers/pages/my/flashcards/edit.js')).default)
-router.get('/my/collection:id/play', (await import('./controllers/pages/my/collections/play.js')).default)
-router.get('/my/play', (await import('./controllers/pages/my/play.js')).default)
+
+// PAGES | PROFILE | AUTH REQUIRED
+router.get('/my/profile', authenticateUser('html'), (await import('./controllers/pages/my/profile/show.js')).default)
+router.get('/my/profile/edit', authenticateUser('html'), (await import('./controllers/pages/my/profile/edit.js')).default)
+
+// PAGES | COLLECTIONS | AUTH REQUIRED
+router.get('/my/collections', authenticateUser('html'), (await import('./controllers/pages/my/collections/show.js')).default)
+router.get('/my/collections/new', authenticateUser('html'), (await import('./controllers/pages/my/collections/new.js')).default)
+router.get('/my/collections/:id/edit', authenticateUser('html'), (await import('./controllers/pages/my/collections/edit.js')).default)
+
+// PAGES | FLASHCARDS | AUTH REQUIRED
+router.get('/my/collections/:id/flashcards', authenticateUser('html'), (await import('./controllers/pages/my/flashcards/show.js')).default)
+router.get('/my/collections/:id/new', authenticateUser('html'), (await import('./controllers/pages/my/flashcards/new.js')).default)
+router.get('/flashcards/:id/edit', authenticateUser('html'), (await import('./controllers/pages/my/flashcards/edit.js')).default)
+
+// PAGES | PLAY | AUTH REQUIRED
+router.get('/my/collection:id/play', authenticateUser('html'), (await import('./controllers/pages/my/collections/play.js')).default)
+router.get('/my/play', authenticateUser('html'), (await import('./controllers/pages/my/play.js')).default)
+
+// PAGES | PAGE NOT FOUND
+router.use((await import('./controllers/pages/not-found.js')).default)
 
 export default router
